493b44c02d7a3914990c19dd35880d42f9f4167b
Karina <lyakhova_karina@list.ru>
2023-03-19 19:40:40 +0300

    поправки

--- ls3.py
+++ ls3.py
@@ -1,3 +1,3 @@
 n = int(input("Введите число - "))
-sum = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
+summa = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
 print("Сумма чисел n + nn + nnn - %d" % sum)
