06a4424b4995df6443e43f78a0477cc148ca0bc9
Karina <lyakhova_karina@list.ru>
2023-03-19 20:08:44 +0300

    поправки 3

--- d8.py
+++ d8.py
@@ -2,6 +2,6 @@ n = int(input("Введите число n: "))
 m = int(input("Введите число m: "))
 k = int(input("Введите число k: "))
 if k < n * m and ((k % n == 0) or (k % m == 0)):
-    print('YES')
+    print("YES")
 else:
-    print('NO')
+    print("NO")
